8c76835d1251e481f3b624e3c3e5a5894afbc3f3
tuleugarin <88728140+tuleugarin@users.noreply.github.com>
2021-08-11 00:55:48 +0500

    10

--- task_3.php
+++ task_3.php
@@ -1,14 +1,33 @@
-<?php       include 'head.php' ?>
+<?php       include 'header.php'; ?>
         <?php
-            $way = [ "Главная", "PHP", "Функции" ];
+            $links = [
+                [
+                    "title" => "Главная",
+                    "href" => "https://www.youtube.com/watch?v=x7CRfL3aXiw&list=PLhqtrZ34-3cYew-CBIbZzVtybwIv3nbnJ",
+                    "is_link" => true
+                ],
+
+                [
+                    "title" => "PHP",
+                    "href" => "https://youtu.be/AtTGflZX3Ks",
+                    "is_link" => true
+                ],
+
+                [
+                    "title" => "Функции",
+                    "href" => "",
+                    "is_link" => false
+                ],
+            ];
         ?>
-              <?php foreach ($way as $road):?>
                             <ol class="breadcrumb page-breadcrumb">
-
-                                <li class="breadcrumb-item"><a href="#">1111</a></li>
-                                <li class="breadcrumb-item"><a href="#">PHP</a></li>
-                                <li class="breadcrumb-item active">222</li>
-
+                                <?php foreach ($links as $link):?>
+                                    <?php if ($link['is_link'] == true): ?>
+                                <li class="breadcrumb-item"><a href="<?php echo $link['href']; ?>">
+                                        <?php echo $link['title']; ?></a></li>
+                                    <?php else: ?>
+                                <li class="breadcrumb-item active"><?php echo $link['title']; ?></li>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
                             </ol>
- <?php endforeach; ?>
-<?php       include 'footer.php' ?>
+<?php       include 'footer.php'; ?>
